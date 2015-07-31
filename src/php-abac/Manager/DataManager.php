9f70816d00fa0b44f827aa3a54ee73a6debe5281
Axel Venet <avenet@kilix.fr>
2015-07-31 10:02:52 +0200

    Add transaction handling in the data Manager

--- src/php-abac/Manager/DataManager.php
+++ src/php-abac/Manager/DataManager.php
@@ -14,6 +14,36 @@ class DataManager {
     }
     
     /**
+     * Start SQL Transaction
+     * Return true in case of success, false otherwise
+     * 
+     * @return boolean
+     */
+    public function beginTransaction() {
+        return $this->connection->beginTransaction();
+    }
+    
+    /**
+     * Commit a SQL Transaction
+     * Return true in case of success, false otherwise
+     * 
+     * @return boolean
+     */
+    public function commitTransaction() {
+        return $this->connection->commit();
+    }
+    
+    /**
+     * Rollback a SQL Transaction
+     * Return true in case of success, false otherwise
+     * 
+     * @return boolean
+     */
+    public function rollbackTransaction() {
+        return $this->connection->rollback();
+    }
+    
+    /**
      * @param string $query
      * @param array $params
      * @return \PDOStatement
