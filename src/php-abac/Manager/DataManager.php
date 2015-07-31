9f945b11499054b8cb138e0aea98c50ab959ddae
Axel Venet <avenet@kilix.fr>
2015-07-31 09:39:41 +0200

    Add insert method to the data manager

--- src/php-abac/Manager/DataManager.php
+++ src/php-abac/Manager/DataManager.php
@@ -13,9 +13,24 @@ class DataManager {
         $this->connection = $connection;
     }
     
+    /**
+     * @param string $query
+     * @param array $params
+     * @return \PDOStatement
+     */
     public function fetchQuery($query, $params) {
         $statement = $this->connection->prepare($query);
         $statement->execute($params);
         return $statement;
+    }
+    
+    /**
+     * @param string $query
+     * @param array $params
+     * @return boolean
+     */
+    public function insertQuery($query, $params) {
+        $statement = $this->connection->prepare($query);
+        return $statement->execute($params);
     }
 }
