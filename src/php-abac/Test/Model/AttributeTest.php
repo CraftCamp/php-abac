53967e4cb76e60498a987b2dadd5d03bbc15735b
Axel Venet <avenet@kilix.fr>
2015-08-03 17:08:29 +0200

    Test new Attribute fields

--- src/php-abac/Test/Model/AttributeTest.php
+++ src/php-abac/Test/Model/AttributeTest.php
@@ -8,15 +8,21 @@ class AttributeTest extends \PHPUnit_Framework_TestCase {
     public function testEntity() {
         $attribute =
             (new Attribute())
+            ->setName('test-attribute')
             ->setColumn('attribute_column')
             ->setTable('user_attributes')
-            ->setIdColumn('user_id')
+            ->setCriteriaColumn('user_id')
             ->setValue(3)
+            ->setCreatedAt(new \DateTime())
+            ->setUpdatedAt(new \DateTime())
         ;
         $this->assertNull($attribute->getId());
+        $this->assertEquals('test-attribute', $attribute->getName());
         $this->assertEquals('attribute_column', $attribute->getColumn());
         $this->assertEquals('user_attributes', $attribute->getTable());
-        $this->assertEquals('user_id', $attribute->getIdColumn());
+        $this->assertEquals('user_id', $attribute->getCriteriaColumn());
+        $this->assertInstanceOf('DateTime', $attribute->getCreatedAt());
+        $this->assertInstanceOf('DateTime', $attribute->getUpdatedAt());
         $this->assertEquals(3, $attribute->getValue());
     }
 }
