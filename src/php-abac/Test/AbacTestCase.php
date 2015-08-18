ba2d14161eed28996aa066be29d00a552b387aa4
Axel Venet <kern046@gmail.com>
2015-08-18 14:37:49 +0200

    Add test case class to load fixtures

---
+++ src/php-abac/Test/AbacTestCase.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace PhpAbac\Test;
+
+use PhpAbac\Abac;
+
+class AbacTestCase extends \PHPUnit_Framework_TestCase {
+    public function loadFixture($fixture) {
+        if(!is_file(__DIR__ . "/fixtures/$fixture.sql")) {
+            throw new \InvalidArgumentException("The asked fixture file $fixture.sql does not exists");
+        }
+        Abac::get('pdo-connection')->exec(file_get_contents(__DIR__ . "/fixtures/$fixture.sql"));
+    }
+}
