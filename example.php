5f8fa8ad1b7db6dae232beedd500bee0c275eaf7
Axel Venet <kern046@gmail.com>
2015-08-27 10:25:08 +0200

    Make an example file with hardcoded values for the POC

---
+++ example.php
@@ -0,0 +1,49 @@
+<?php
+
+    require_once('vendor/autoload.php');
+
+    use PhpAbac\Abac;
+
+    $abac = new Abac(new \PDO(
+        'mysql:host=localhost;dbname=php_abac_test',
+        'root',
+        'vagrant',
+        [
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+        ]
+    ));
+    Abac::get('pdo-connection')->exec(file_get_contents("src/php-abac/Test/fixtures/policy_rules.sql"));
+    
+    $user1Nationality = $abac->enforce('nationality-access', 1);
+    
+    if($user1Nationality) {
+        echo("GRANTED : The user 1 is able to be nationalized\n");
+    } else {
+        echo("FAIL : The system didn't grant access\n");
+    }
+    
+    $user2Nationality = $abac->enforce('nationality-access', 2);
+    if(!$user2Nationality) {
+        echo("DENIED : The user 2 is not able to be nationalized because he hasn't done his JAPD\n");
+    } else {
+        echo("FAIL : The system didn't deny access\n");
+    }
+
+    $user1Vehicle = $abac->enforce('vehicle-homologation', 1, 1);
+    if($user1Vehicle) {
+        echo("GRANTED : The vehicle 1 is able to be approved for the user 1\n");
+    } else {
+        echo("FAIL : The system didn't grant access\n");
+    }
+    $user3Vehicle = $abac->enforce('vehicle-homologation', 3, 2);
+    if(!$user3Vehicle) {
+        echo("DENIED : The vehicle 2 is not approved for the user 3 because its last technical review is too old\n");
+    } else {
+        echo("FAIL : The system didn't deny access\n");
+    }
+    $user4Vehicle = $abac->enforce('vehicle-homologation', 4, 4);
+    if(!$user4Vehicle) {
+        echo("DENIED : The vehicle 4 is not able to be approved for the user 4 because he has no driving license\n");
+    } else {
+        echo("FAIL : The system didn't deny access\n");
+    }
