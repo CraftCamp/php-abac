81826809547106be02408df1ea9f8e98fd1dac88
Axel Venet <kern046@gmail.com>
2016-10-03 08:41:48 +0200

    Implement tests for user and resource comparisons

---
+++ tests/Comparison/ObjectComparisonTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace PhpAbac\Test\Comparison;
+
+use PhpAbac\Comparison\ObjectComparison;
+
+use Symfony\Component\Config\FileLocator;
+
+use PhpAbac\Manager\ConfigurationManager;
+use PhpAbac\Manager\ComparisonManager;
+use PhpAbac\Manager\AttributeManager;
+
+use PhpAbac\Model\PolicyRuleAttribute;
+use PhpAbac\Model\Attribute;
+
+use PhpAbac\Example\User;
+use PhpAbac\Example\Vehicle;
+
+class ObjectComparisonTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var ArrayComparison **/
+    protected $comparison;
+
+    public function setUp()
+    {
+        $configuration = new ConfigurationManager(new FileLocator());
+        $configuration->parseConfigurationFile([__DIR__.'/../fixtures/policy_rules.yml']);
+
+        $this->comparison = new ObjectComparison(new ComparisonManager(new AttributeManager($configuration->getAttributes())));
+    }
+
+    public function testIsFieldEqual() {
+        $countries = include(__DIR__ . '/../fixtures/countries.php');
+        $visas = include(__DIR__ . '/../fixtures/visas.php');
+        $extraData = [
+            'resource' =>
+                (new Vehicle())
+                ->setId(1)
+                ->setOwner((new User())->setId(1))
+        ];
+        $this->assertFalse($this->comparison->isFieldEqual('vehicle.owner.id', 2, $extraData));
+        $this->assertTrue($this->comparison->isFieldEqual('vehicle.owner.id', 1, $extraData));
+    }
+}
