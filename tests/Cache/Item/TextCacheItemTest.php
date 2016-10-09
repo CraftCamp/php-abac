ec010f4ddd6509787e27596d3ddf11d47d5f771f
Axel Venet <kern046@gmail.com>
2016-10-09 17:05:32 +0200

    Implement tests for text caching

---
+++ tests/Cache/Item/TextCacheItemTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace PhpAbac\Test\Cache\Item;
+
+use PhpAbac\Cache\Item\TextCacheItem;
+
+class TextCacheItemTest extends \PHPUnit_Framework_TestCase {
+    /** @var \PhpAbac\Cache\Item\TextCacheItem **/
+    protected $item;
+
+    public function setUp() {
+        $this->item = new TextCacheItem('php_abac.test');
+    }
+
+    public function testSet() {
+        $this->item->set('test');
+
+        $this->assertEquals('test', $this->item->get());
+    }
+
+    public function testIsHit() {
+        $this->assertTrue($this->item->isHit());
+    }
+
+    public function testIsHitWithMissItem() {
+        $this->item->expiresAt((new \DateTime())->setTimestamp(time() - 100));
+
+        $this->assertFalse($this->item->isHit());
+    }
+
+    public function testGetKey() {
+        $this->assertEquals('php_abac.test', $this->item->getKey());
+    }
+
+    public function testGet() {
+        $this->item->set('test');
+
+        $this->assertEquals('test', $this->item->get());
+    }
+
+    public function testExpiresAt() {
+        $time = time();
+
+        $this->item->expiresAt((new \DateTime())->setTimestamp($time));
+
+        $this->assertEquals($time, $this->item->getExpirationDate()->getTimestamp());
+    }
+
+    public function testExpiresAfter() {
+        $time = time() + 1500;
+
+        $this->item->expiresAfter(1500);
+
+        $this->assertEquals($time, $this->item->getExpirationDate()->getTimestamp());
+    }
+}
