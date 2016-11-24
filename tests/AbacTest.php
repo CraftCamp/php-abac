c9e82dd35173fabfbafefa29d0dc6accca48b6a7
Mathieu Vedie <m.vedie@dreamcentury.com>
2016-11-24 23:24:13 +0100

    Adding Test for JSON

--- tests/AbacTest.php
+++ tests/AbacTest.php
@@ -1,17 +1,22 @@
 <?php
-
 namespace PhpAbac\Test;
+
+date_default_timezone_set('UTC');
+
 
 use PhpAbac\Abac;
 
 class AbacTest extends \PHPUnit_Framework_TestCase
 {
-    /** @var Abac **/
-    protected $abac;
+    /** @var Abac[] $abac_a * */
+    protected $abac_a;
 
     public function setUp()
     {
-        $this->abac = new Abac([__DIR__ . '/fixtures/policy_rules.yml']);
+        $this->abac_a = [
+            new Abac([__DIR__ . '/fixtures/policy_rules.yml']),
+            new Abac([__DIR__ . '/fixtures/policy_rules.json'])
+        ];
     }
 
     public function testEnforce()
@@ -21,31 +26,36 @@ class AbacTest extends \PHPUnit_Framework_TestCase
         $users = include('tests/fixtures/users.php');
         $vehicles = include('tests/fixtures/vehicles.php');
 
-        $this->assertTrue($this->abac->enforce('nationality-access', $users[3]));
-        $this->assertEquals([
-            'japd',
-        ], $this->abac->enforce('nationality-access', $users[1]));
+        foreach ($this->abac_a as $abac) {
 
-        // getenv() don't work in CLI scripts without putenv()
-        putenv('SERVICE_STATE=OPEN');
+            $this->assertTrue($abac->enforce('nationality-access', $users[3]));
+            $this->assertEquals([
+                'japd',
+            ], $abac->enforce('nationality-access', $users[1]));
 
-        $this->assertTrue($this->abac->enforce('vehicle-homologation', $users[0], $vehicles[0]));
-        $this->assertEquals(
-            ['derniere-revision-technique'],
-            $this->abac->enforce('vehicle-homologation', $users[2], $vehicles[1])
-        );
-        $this->assertEquals(
-            ['permis-de-conduire'],
-            $this->abac->enforce('vehicle-homologation', $users[3], $vehicles[3])
-        );
-        $this->assertEquals(
-            ['visas'],
-            $this->abac->enforce('travel-to-foreign-country', $users[0], null, [
+            // getenv() don't work in CLI scripts without putenv()
+            putenv('SERVICE_STATE=OPEN');
+
+            $this->assertTrue($abac->enforce('vehicle-homologation', $users[0], $vehicles[0]));
+            $this->assertEquals(
+                ['derniere-revision-technique'],
+                $abac->enforce('vehicle-homologation', $users[2], $vehicles[1])
+            );
+            $this->assertEquals(
+                ['permis-de-conduire'],
+                $abac->enforce('vehicle-homologation', $users[3], $vehicles[3])
+            );
+            $this->assertEquals(
+                ['visas'],
+                $abac->enforce('travel-to-foreign-country', $users[0], null, [
+                    'dynamic_attributes' => ['code-pays' => 'US']
+                ])
+            );
+            $this->assertTrue($abac->enforce('travel-to-foreign-country', $users[1], null, [
                 'dynamic_attributes' => ['code-pays' => 'US']
-            ])
-        );
-        $this->assertTrue($this->abac->enforce('travel-to-foreign-country', $users[1], null, [
-            'dynamic_attributes' => ['code-pays' => 'US']
-        ]));
+            ]));
+        }
     }
+
+
 }
